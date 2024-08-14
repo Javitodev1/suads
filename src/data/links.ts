a5ed18191a2dd249cf4c525dd0a84850e00dd660
Javitodev <javito.dev@gmail.com>
2024-08-14 19:47:06 -0400

    website links exported as js object

---
+++ src/data/links.ts
@@ -0,0 +1,15 @@
+export const PAGES = {
+  index: '/',
+  contact: '/contacto',
+}
+
+export const HOME_ANCHORS = {
+  hero: '/#hero',
+  services: '/#services',
+  creativity: '/#creativity',
+  adMethod: '/#ad-method',
+  salesMethod: '/#sales-method',
+  adProduction: '/#ad-production',
+  webDesign: '/#web-design',
+  testimonies: '/#testimonies',
+}
