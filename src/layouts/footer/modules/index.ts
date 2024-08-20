8772c26ea0d27edebf29a7174d816fa3b2630ced
Javier-Desktop <jftes.dev@outlook.es>
2024-08-20 01:21:37 -0400

    footer and header refactorized and divided into smaller components

---
+++ src/layouts/footer/modules/index.ts
@@ -0,0 +1,5 @@
+import FooterContainer from './FooterContainer.astro'
+import CtaContainer from './CtaContainer.astro'
+import ContentContainer from './ContentContainer.astro'
+
+export { FooterContainer, CtaContainer, ContentContainer }
---
+++ src/layouts/header/modules/index.ts
@@ -0,0 +1,9 @@
+import HeaderContainer from './HeaderContainer.astro'
+import Navbar from './Navbar.astro'
+import MediaContainer from './MediaContainer.astro'
+import MediaItem from './MediaItem.astro'
+import ResponsiveContainer from './ResponsiveContainer.astro'
+import MenuContainer from './MenuContainer.astro'
+import NavLink from './NavLink.astro'
+
+export { HeaderContainer, Navbar, MediaContainer, MenuContainer, NavLink, ResponsiveContainer, MediaItem }
