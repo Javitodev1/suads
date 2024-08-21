3a9295f8c30b0d6b0d84869f508132a17f5435aa
Javitodev <javito.dev@gmail.com>
2024-08-21 13:07:48 -0400

    fixed whitespace in home content

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -6,6 +6,7 @@ enum Colors {
   neutral_white_less,
   accent_title,
   accent_cta,
+  neutral_white_secondary,
 }
 
 const TextColors: Record<Colors, string> = {
@@ -16,6 +17,7 @@ const TextColors: Record<Colors, string> = {
   [Colors.neutral_white_less]: 'text-neutral-white-less',
   [Colors.accent_title]: 'text-accent-title',
   [Colors.accent_cta]: 'text-accent-cta',
+  [Colors.neutral_white_secondary]: 'text-neutral-100',
 }
 
 const BorderColors: Record<Colors, string> = {
@@ -26,6 +28,7 @@ const BorderColors: Record<Colors, string> = {
   [Colors.neutral_white_less]: 'border-neutral-white-less',
   [Colors.accent_title]: 'border-accent-title',
   [Colors.accent_cta]: 'border-accent-cta',
+  [Colors.neutral_white_secondary]: 'border-neutral-100',
 }
 
 const BackgroundColors: Record<Colors, string> = {
@@ -36,6 +39,7 @@ const BackgroundColors: Record<Colors, string> = {
   [Colors.neutral_white_less]: 'bg-neutral-white-less',
   [Colors.accent_title]: 'bg-accent-title',
   [Colors.accent_cta]: 'bg-accent-cta',
+  [Colors.neutral_white_secondary]: 'bg-neutral-100',
 }
 
 export type Color = keyof typeof Colors
