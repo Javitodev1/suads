eca08549665d05681d85809dc31d6023674e13d0
Javitodev <javito.dev@gmail.com>
2024-08-20 22:08:18 -0400

    footer refactorized

--- src/layouts/footer/modules/index.ts
+++ src/layouts/footer/modules/index.ts
@@ -1,5 +1,8 @@
 import FooterContainer from './FooterContainer.astro'
 import CtaContainer from './CtaContainer.astro'
 import ContentContainer from './ContentContainer.astro'
+import LinkGrid from './LinkGrid.astro'
+import GridColumn from './GridColumn.astro'
+import GridItem from './GridItem.astro'
 
-export { FooterContainer, CtaContainer, ContentContainer }
+export { FooterContainer, CtaContainer, ContentContainer, LinkGrid, GridColumn, GridItem }
