18cd089b2919d37186e8db9490e2f2c1ab9876cf
Javitodev <javito.dev@gmail.com>
2024-10-01 14:55:36 -0300

    remove un used packages and files, rendered globe was removed from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
-  output: 'static'
-});
+  integrations: [tailwind()],
+  output: 'static',
+})
